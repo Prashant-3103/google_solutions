09cf71449068fea1f8021a9dbc65f87f6df82f0e
Prashant-3103 <prashantkumarchoudhary.211mn035@nitk.edu.in>
2024-01-10 21:35:36 +0530

    add header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Footer from '@/components/ui/Footer'
+import Header from '@/components/ui/Header'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -19,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-
+<Header/>
         {children}
 <Footer/>
         </body>
