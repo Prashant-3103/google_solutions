bd9fa4a9428c6f1d84f71916564a28a673a6b337
Prashant-3103 <prashantkumarchoudhary.211mn035@nitk.edu.in>
2024-01-11 12:03:22 +0530

    fixed features and landing

--- components/ui/Features.tsx
+++ components/ui/Features.tsx
@@ -1,41 +1,49 @@
-import Image from "next/image"
-const Features =()=>{
-return(
-    <section className="container mx-auto px-6 p-10 bg-[]">
+import Image from "next/image";
+
+const Features = () => {
+  return (
+    <section className="container mx-auto px-6 p-10">
       <h2 className="text-4xl font-bold text-center text-gray-800 mb-8">
         Features
       </h2>
-      <div className="flex items-center flex-wrap mb-20">
-        <div className="w-full md:w-1/2">
-          <h4 className="text-3xl text-gray-800 font-bold mb-3">Exercise Metric</h4>
-          <p className="text-gray-600 mb-8">Our Smart Health Monitoring Wristwatch is able to capture you vitals while you exercise. You can create different category of exercises and can track your vitals on the go.</p>
-        </div>
-        <div className="w-full md:w-1/2">
+      <div className="flex items-center flex-wrap mb-8 md:w-[70%] lg:w-[70%]">
+      <div className="w-full md:w-1/2">
           <img src="./image1.jpeg" alt="Monitoring" />
         </div>
+        <div className="w-full md:w-1/2 mb-4 md:mb-0">
+          <h4 className="text-3xl text-gray-800 font-bold mb-3 md:items-center">Exercise Metric</h4>
+          <p className="text-gray-600 mb-8">
+            Our Smart Health Monitoring Wristwatch is able to capture your vitals while you exercise. You can create different categories of exercises and can track your vitals on the go.
+          </p>
+        </div>
       </div>
 
-      <div className="flex items-center flex-wrap mb-20">
-        <div className="w-full md:w-1/2">
+      <div className="flex items-center flex-wrap mb-8 md:w-[70%] lg:w-[70%] md:mx-80">
+        <div className="w-full md:w-1/2 mb-4 md:mb-0">
           <img src="./image2.jpeg" alt="Reporting" />
         </div>
-        <div className="w-full md:w-1/2 pl-10">
+        <div className="w-full md:w-1/2 pl-0 md:pl-10">
           <h4 className="text-3xl text-gray-800 font-bold mb-3">Reporting</h4>
-          <p className="text-gray-600 mb-8">Our Smart Health Monitoring Wristwatch can generate a comprehensive report on your vitals depending on your settings either daily, weekly, monthly, quarterly or yearly.</p>
+          <p className="text-gray-600 mb-8">
+            Our Smart Health Monitoring Wristwatch can generate a comprehensive report on your vitals depending on your settings either daily, weekly, monthly, quarterly, or yearly.
+          </p>
         </div>
       </div>
 
-      <div className="flex items-center flex-wrap mb-20">
-        <div className="w-full md:w-1/2">
-          <h4 className="text-3xl text-gray-800 font-bold mb-3">Syncing</h4>
-          <p className="text-gray-600 mb-8">Our Smart Health Monitoring Wristwatch allows you to sync data across all your mobile devices whether iOS, Android or Windows OS and also to your laptop whether MacOS, GNU/Linux or Windows OS.</p>
-        </div>
-        <div className="w-full md:w-1/2">
+      <div className="flex items-center flex-wrap md:w-[70%] lg:w-[70%] ">
+      <div className="w-full md:w-1/2">
           <img src="image2.jpeg" alt="Syncing" />
         </div>
+        <div className="w-full md:w-1/2 mb-4 md:mb-0">
+          <h4 className="text-3xl text-gray-800 font-bold mb-3">Syncing</h4>
+          <p className="text-gray-600 mb-8">
+            Our Smart Health Monitoring Wristwatch allows you to sync data across all your mobile devices whether iOS, Android, or Windows OS and also to your laptop whether MacOS, GNU/Linux, or Windows OS.
+          </p>
+        </div>
+
       </div>
     </section>
-)
-}
+  );
+};
 
-export default Features
+export default Features;
--- components/ui/Hero.tsx
+++ components/ui/Hero.tsx
@@ -17,7 +17,12 @@ const Hero = () => {
             <div className='relative'>
 
             </div>
-            <button className='w-full bg-primary text-white font-semibold rounded-lg px-5 py-3 '>Community</button>
+           <div className=" flex flex-wrap gap-x-4 gap-y-2.5 lg:text-sm xl:text-base ">
+           <button className='border-2 mt-5 lg:mt-0 border-blue-500 px-6 py-2 rounded-full text-full-500 font-semibold hover:bg-dark-spansoft hover:text-white transition-all duration-300 '>Community</button>
+            <button className='border-2 mt-5 lg:mt-0 border-blue-500 px-6 py-2 rounded-full text-full-500 font-semibold hover:bg-dark-spansoft hover:text-white transition-all duration-300 '>Chat Bot</button>
+            <button className='border-2 mt-5 lg:mt-0 border-blue-500 px-6 py-2 rounded-full text-full-500 font-semibold hover:bg-dark-spansoft hover:text-white transition-all duration-300 '>Talk to one of us</button>
+           </div>
+
           </div>
 
 
